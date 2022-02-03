ed3bf6f5c740c42a4480acd0be3f05551a8f8d49
João Henrique <joaohenriquetdj@gmail.com>
2022-02-03 17:04:19 -0300

    create project

---
+++ src/main/java/com/tranporte_cargas/TranporteCargasApplication.java
@@ -0,0 +1,13 @@
+package com.tranporte_cargas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TranporteCargasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TranporteCargasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tranporte_cargas/TranporteCargasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tranporte_cargas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TranporteCargasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
